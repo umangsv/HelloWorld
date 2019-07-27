8b06470e68a8d736ab17678c708f8abbec967e18
User <User@192.168.1.12>
2019-07-27 23:37:05 +0530

    HelloWorld Jenkins

---
+++ HelloWorldForJenkins/src/MyPackage4Jenkins/HelloWorldUAS.java
@@ -0,0 +1,16 @@
+package MyPackage4Jenkins;
+
+public class HelloWorldUAS {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i=1; i<=10; i++)
+		{
+			System.out.println("Hello World!!!!!!" + i);
+		}
+				
+
+	}
+
+}
